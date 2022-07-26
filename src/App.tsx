564f724ab8bbec280b031529a30f0fa298c19b54
Sean Kim <thesean.kim33@gmail.com>
2022-07-26 15:58:54 -0500

    feat: Remove tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Install & Setup Vite + React + Typescript + Tailwind CSS 3
-      </h1>
+    <div>
+      <h1>Vite + React</h1>
     </div>
   );
 }
