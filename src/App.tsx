5a4cd50c79690472171dae09b2c24423025432d4
Sean Kim <thesean.kim33@gmail.com>
2022-08-02 16:11:34 -0500

    feat: add Company Logo Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 // import Hero from "./components/dictionary/Hero";
 import HeroPhoto from "./components/HeroPhoto";
+import CompanyLogoSection from "./components/CompanyLogoSection";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       {/* <Hero /> */}
       <HeroPhoto />
+      <CompanyLogoSection />
     </div>
   );
 };
