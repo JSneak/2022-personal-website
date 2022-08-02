42fde197caa1f0b17c4a64ec8260ad6845bb0159
Sean Kim <thesean.kim33@gmail.com>
2022-08-02 00:31:18 -0500

    Revert "feat: Remove tailwindcss"
    This reverts commit 564f724ab8bbec280b031529a30f0fa298c19b54.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Vite + React</h1>
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
     </div>
   );
 }
