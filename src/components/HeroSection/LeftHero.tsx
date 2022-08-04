bea232f9a97722fc9ab7627a906a8ed7751bb187
Sean Kim <thesean.kim33@gmail.com>
2022-08-04 09:38:53 -0500

    feat: fix spacing on images

--- src/components/HeroSection/LeftHero.tsx
+++ src/components/HeroSection/LeftHero.tsx
@@ -1,6 +1,6 @@
 const LeftHero = () => {
   return (
-    <div className={"flex flex-col basis-1/2 justify-center"}>
+    <div className={"flex flex-col grow sm:basis-1/2 justify-center"}>
       <p className="text-slate-300 text-4xl">Hello, I'm</p>
       <h1 className="text-white font-bold text-6xl">Sean J. Kim</h1>
       <p className={" text-slate-300 text-4xl "}>
--- src/components/HeroSection/RightHero.tsx
+++ src/components/HeroSection/RightHero.tsx
@@ -7,19 +7,41 @@ import Torus from "../../assets/hero/Torus.svg";
 import Torus2 from "../../assets/hero/Torus2.svg";
 
 const RightHero = () => {
-  // <img src={Profile} alt={"Profile"} className={"h-[32rem]"} />;
   return (
-    <div className={" flex basis-1/2 hidden sm:block h-[32rem] w-full relative"}>
-      {/* <img
-        src={Sean}
-        alt={"Profile"}
-        className={"absolute h-[30rem] bottom-0 z-10"}
-      /> */}
-      <div className={"absolute h-[100%] w-[100%] bottom-0 z-10 bg-contain bg-bottom bg-no-repeat bg-hero-profile"}></div>
+    <div
+      className={
+        "hidden sm:flex basis-1/2x h-[32rem] w-full relative overflow-hidden"
+      }
+    >
+      <div
+        className={
+          "absolute h-[100%] w-[100%] bottom-0 z-30 bg-contain bg-bottom bg-no-repeat bg-hero-profile"
+        }
+      />
+      <img
+        src={PinkBlob}
+        alt={"Pink Blob"}
+        className={"absolute h-[100%] w-[100%] z-10 -bottom-36"}
+      />
       <img
         src={OrangeBlob}
         alt={"Orange Blob"}
-        className={"absolute h-full z-0"}
+        className={"absolute h-[100%] w-[50%] z-20 right-20 -top-28"}
+      />
+      <img
+        src={Rock2}
+        alt={"Orange Blob"}
+        className={"absolute h-[100%] w-[15%] z-40 bottom-0"}
+      />
+      <img
+        src={Torus}
+        alt={"Orange Blob"}
+        className={"absolute h-[100%] w-[15%] z-40 -bottom-48 right-10"}
+      />
+      <img
+        src={Torus2}
+        alt={"Orange Blob"}
+        className={"absolute h-[20%] w-[40%] z-40 top-10 right-10"}
       />
     </div>
   );
