2d520ac00bd493eee8d8ad339c90f8222f3f4c2d
Sean Kim <thesean.kim33@gmail.com>
2022-08-07 18:54:12 -0500

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePluginFonts } from "vite-plugin-fonts";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({ registerType: "autoUpdate" }),
     react({ jsxRuntime: "classic" }),
     VitePluginFonts({
       google: {
